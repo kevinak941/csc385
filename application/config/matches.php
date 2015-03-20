429150d403189f90c731a4854ce5d86f0ee303e1
Kevin <kevinak941@gmail.com>
2015-03-20 11:46:35 -0400

    Update configuration files

---
+++ application/config/matches.php
@@ -0,0 +1,7 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * Config file for core matching
+ */
+ 
+$config['matchingConditions'] = array('product_id', 'condition', 'title', 'subtitle', 'image', 'selling_state', 'buy_it_now_price');
+$config['hideMatches'] = array('image');

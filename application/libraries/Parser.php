e744b7c58448a7258b6b93d387dd9119175985d9
Kevin <kevinak941@gmail.com>
2015-04-27 15:07:56 -0400

    Enhance parser

--- application/libraries/Parser.php
+++ application/libraries/Parser.php
@@ -16,28 +16,34 @@ class Parser extends Connector{
     public $totalItems = 0;
     public $totalLocal = 0;
     public $totalRemote = 0;
+    public $totalRemoved = 0;
     public $firstStartDate = NULL;
     public $lastStartDate = NULL;
     
     // Collections
-    public $items = array();
-    public $byCategory = array();
-    public $pricesByCategory = array();
-    public $byPrice = array();
-    public $mostCommon = array();
-    public $itemIds = array();
+    public $items           = array();
+    public $byCategory      = array();
+    public $pricesByCategory= array();
+    public $byPrice         = array();
+    public $mostCommon      = array();
+    public $itemIds         = array();
+    public $removedItems    = array();
     
     
-    public $matchedProductId = NULL;
-    public $storedTags = array();
-    public $trackCommon = array();
+    public $matchedProductId= NULL;
+    public $storedTags      = array();
+    public $trackCommon     = array();
     
     // Internal Cache
-    private $_currentPrice = 0;
-    private $_currentId = 0;
+    private $_currentItem   = false;
+    private $_currentDate   = null;
+    private $_currentPrice  = 0;
+    private $_currentId     = 0;
     private $_currentItemId = 0;
+    private $_tag_outliers  = array();
     
-    private $_tag_types = array();
+    private $_tag_types         = array();
+    private $_weightedKeywords  = array();
     
     public function __construct() {
         parent::__construct();
@@ -45,6 +51,16 @@ class Parser extends Connector{
         // Load in tag types
         if(empty($this->_tag_types)) 
             $this->_tag_types = $this->ci->tag_type_m->get(array('array_key'=>'name'));
+    }
+    
+    public function loadKeyword($keyword = NULL) {
+        if($keyword) {
+            $ex = explode(' ', $keyword);
+            foreach($ex as $tag) {
+                if($tag != '' && strpos($tag, '*') === 0)
+                    $this->_weightedKeywords[] = substr(strtolower($tag), 1, strlen($tag));
+            }
+        }
     }
     
     /**
@@ -57,77 +73,95 @@ class Parser extends Connector{
         if(!empty($items)) {
             // Loop through the items performing parse
             foreach($items as $key => $item) {
-                if(is_array($item)){
-                    // Parsing database content
-                    // TODO: Auto assign item variables to commons
-                    $product_id = $item['site_product_id'];
-                    $condition = $item['condition'];
-                    $title = $item['title'];
-                    $subtitle = $item['subtitle'];
-                    $listing_type = $item['type'];
-                    $buy_it_now_price = $item['buyItNowPrice'];
-                    $selling_state = $item['sellingState'];
-                    $images = $item['image'];
-                    $shippingServiceCost = $item['shippingServiceCost'];
-                    $shippingType = $item['shippingType'];
-                    $shipToLocations = $item['shipToLocations'];
-                    $topRatedListing = $item['topRatedListing'];
-                    $this->_currentPrice = $item['currentPrice'];
-                    $this->_currentItemId   =   $item['site_item_id'];
-                    $this->totalLocal++;
-                    
-                    if($this->firstStartDate==NULL||$item['startTime']<$this->firstStartDate) $this->firstStartDate = $item['startTime'];
-                    if($this->lastStartDate==NULL||$item['startTime']>$this->lastStartDate)  $this->lastStartDate = $item['startTime'];
-                } else {
-                    // Parsing data from external source
-                    // By Category
-                    if($item->primaryCategory) {
-                        $cat_string = (string)$item->primaryCategory->categoryId;
-                        if(!isset($this->pricesByCategory[$cat_string])) $this->pricesByCategory[$cat_string] = array();
-                        $this->pricesByCategory[$cat_string][] = (int)$item->sellingStatus->currentPrice;
+                // Get the title match rating for this item
+                $rating = $this->getMatchRating((isset($item['title']) ? $item['title'] : (string)$item->title));
+                // Check if title match is enabled and the item has failed the test
+                if($this->ci->config->item('enable_titleMatching') && $rating < ((array_sum($this->storedTags)*$this->ci->config->item('titleMatching_tolerance'))-count($this->_tag_outliers))) {
+                    // Item did not pass tag calculation
+                    // Only add live data to removed items 
+                    // Note: It would be silly to pull database content then filter it out
+                    if(is_object($items[$key])) {
+                        // Add the item to removed list
+                        $this->removedItems[] = $items[$key];
+                        // Increment Counters
+                        $this->totalItems++;
+                        $this->totalRemoved++;
                     }
-                    // Common Tracking
-                    $product_id             = $item->productId;
-                    $condition              = (string)$item->condition->conditionDisplayName;
-                    $title                  = (string)$item->title;
-                    $subtitle               = (string)$item->subtitle;
-                    $listing_type           = (string)$item->listingInfo->listingType;
-                    $buy_it_now_price       = (string)$item->listingInfo->buyItNowPrice;
-                    $selling_state          = (string)$item->sellingStatus->sellingState;
-                    $shippingServiceCost    = $item->shippingInfo->shippingServiceCost;
-                    $shippingType           = (string)$item->shippingInfo->shippingType;
-                    $shipToLocations        = (string)$item->shippingInfo->shipToLocations;
-                    $topRatedListing        = $item->topRatedListing;
-                    $images                 = (string)$item->galleryURL;
-                    $this->_currentPrice    = (int)$item->sellingStatus->currentPrice;
-                    $this->_currentItemId   = (string)$item->itemId;
+                    unset($items[$key]);
+                } else {
+                    $this->totalItems++;
+                    if(is_array($item)){
+                        // Parsing database content
+                        // TODO: Auto assign item variables to commons
+                        $product_id             = $item['site_product_id'];
+                        $condition              = $item['condition'];
+                        $title                  = $item['title'];
+                        $subtitle               = $item['subtitle'];
+                        $type                   = $item['type'];
+                        $buyItNowPrice          = $item['buyItNowPrice'];
+                        $sellingState           = $item['sellingState'];
+                        $image                  = $item['image'];
+                        $shippingServiceCost    = $item['shippingServiceCost'];
+                        $shippingType           = $item['shippingType'];
+                        $shipToLocations        = $item['shipToLocations'];
+                        $topRatedListing        = $item['topRatedListing'];
+                        $this->_currentPrice    = $item['currentPrice'];
+                        $this->_currentItemId   =   $item['site_item_id'];
+                        $this->_currentDate     = $item['endTime'];
+                        $this->totalLocal++;
+                        
+                        if($this->firstStartDate==NULL||$item['startTime']<$this->firstStartDate) $this->firstStartDate = $item['startTime'];
+                        if($this->lastStartDate==NULL||$item['startTime']>$this->lastStartDate)  $this->lastStartDate = $item['startTime'];
+                    } else {
+                        // Parsing data from external source
+                        // By Category
+                        if($item->primaryCategory) {
+                            $cat_string = (string)$item->primaryCategory->categoryId;
+                            if(!isset($this->pricesByCategory[$cat_string])) $this->pricesByCategory[$cat_string] = array();
+                            $this->pricesByCategory[$cat_string][] = (int)$item->sellingStatus->currentPrice;
+                        }
+                        // Common Tracking
+                        $product_id             = $item->productId;
+                        $condition              = (string)$item->condition->conditionDisplayName;
+                        $title                  = (string)$item->title;
+                        $subtitle               = (string)$item->subtitle;
+                        $type                   = (string)$item->listingInfo->listingType;
+                        $buyItNowPrice          = (string)$item->listingInfo->buyItNowPrice;
+                        $sellingState           = (string)$item->sellingStatus->sellingState;
+                        $shippingServiceCost    = $item->shippingInfo->shippingServiceCost;
+                        $shippingType           = (string)$item->shippingInfo->shippingType;
+                        $shipToLocations        = (string)$item->shippingInfo->shipToLocations;
+                        $topRatedListing        = $item->topRatedListing;
+                        $image                  = (string)$item->galleryURL;
+                        $this->_currentPrice    = (int)$item->sellingStatus->currentPrice;
+                        $this->_currentItemId   = (string)$item->itemId;
+                        $this->_currentDate     = (string)$item->listingInfo->endTime;
+                        $this->items[]      = $item;
+                        $this->itemIds[]    = (string)$item->itemId;
+                        
+                        $this->totalRemote++;
+                        
+                        if($this->firstStartDate==NULL||$item->listingInfo->startDate<$this->firstStartDate) $this->firstStartDate = $item->listingInfo->startDate;
+                        if($this->lastStartDate==NULL||$item->listingInfo->startDate>$this->lastStartDate)  $this->lastStartDate = $item->listingInfo->startDate;
+                    }
                     
-                    $this->items[]      = $item;
-                    $this->itemIds[]    = (int)$item->itemId;
-                    
-                    $this->totalRemote++;
-                    
-                    if($this->firstStartDate==NULL||$item->listingInfo->startDate<$this->firstStartDate) $this->firstStartDate = $item->listingInfo->startDate;
-                    if($this->lastStartDate==NULL||$item->listingInfo->startDate>$this->lastStartDate)  $this->lastStartDate = $item->listingInfo->startDate;
+                    // Store common values
+                    $this->_addCommon('product_id', $product_id);
+                    $this->_addCommon('condition', $condition);
+                    $this->_addCommon('title', $title);
+                    $this->_addCommon('subtitle', $subtitle);
+                    $this->_addCommon('listing_type', $type);
+                    $this->_addCommon('buy_it_now_price', $buyItNowPrice);
+                    $this->_addCommon('selling_state', $sellingState);
+                    $this->_addCommon('shipping_service_cost', $shippingServiceCost);
+                    $this->_addCommon('ship_to_location', $shipToLocations);
+                    $this->_addCommon('top_rated_listing', $topRatedListing);
+                    $this->_addCommon('selling_state', $sellingState);
+                    $this->_addCommon('images', $image);
                 }
-                
-                $this->explodeTitle($title);
-                // Store common values
-                $this->_addCommon('product_id', $product_id);
-                $this->_addCommon('condition', $condition);
-                $this->_addCommon('title', $title);
-                $this->_addCommon('subtitle', $subtitle);
-                $this->_addCommon('listing_type', $listing_type);
-                $this->_addCommon('buy_it_now_price', $buy_it_now_price);
-                $this->_addCommon('selling_state', $selling_state);
-                $this->_addCommon('shipping_service_cost', $shippingServiceCost);
-                $this->_addCommon('ship_to_location', $shipToLocations);
-                $this->_addCommon('top_rated_listing', $topRatedListing);
-                $this->_addCommon('selling_state', $selling_state);
-                $this->_addCommon('images', $images);
-                $this->totalItems++;
             }
             $this->_buildCommon();
+            
         } else {
         
         }
@@ -174,6 +208,7 @@ class Parser extends Connector{
         if($this->trackCommon[$type][$value]['max']<$this->_currentPrice) $this->trackCommon[$type][$value]['max'] = $this->_currentPrice;
         $this->trackCommon[$type][$value]['itemIds'][] = $this->_currentItemId;
         $this->trackCommon[$type][$value]['ids'][] = $this->_currentId;
+        $this->trackCommon[$type][$value]['date'][] = array('date'=>$this->_currentDate, 'price'=> (int)$this->_currentPrice);
     }
     
     /**
@@ -190,7 +225,7 @@ class Parser extends Connector{
         }
     }
     
-    public function explodeTitle($title) {
+    public function explodeTitle($title, $store = true) {
         $tag_type = $this->_tag_types['title'];
         // Check if tag type requires any chars to be removed
         if(isset($tag_type['remove_chars'])) {
@@ -200,13 +235,36 @@ class Parser extends Connector{
         }
         // Explode tag string by predefined delimiter
         $ex = explode($tag_type['delimiter'], $tags);
+        if($store) {
+            foreach($ex as $tag) {
+                $tag = strtolower($tag);
+                if(!isset($this->storedTags[$tag])) 
+                    $this->storedTags[$tag] = 0;
+
+                $this->storedTags[$tag]++;
+            }
+        }
+        return $ex;
+    }
+    
+    /**
+     * Must be called after stored tags have been calculated by explodeTitle
+     */
+    public function getMatchRating($title) {
+        $ex = $this->explodeTitle($title, false);
+        $rating = 0;
         foreach($ex as $tag) {
             $tag = strtolower($tag);
-            if(!isset($this->storedTags[$tag])) 
-                $this->storedTags[$tag] = 0;
-
-            $this->storedTags[$tag]++;
+            if($tag != '' && isset($this->_weightedKeywords[$tag])) {
+                if(isset($this->storedTags[$tag])) $rating+=($this->storedTags[$tag]*2);
+            } else {
+                if(isset($this->storedTags[$tag])) $rating+=($this->storedTags[$tag] > 1) ? $this->storedTags[$tag] : 0;
+            }
+            if(isset($this->storedTag[$tag]) && $this->storedTags[$tag] < 2) {
+                $this->_tag_outliers[] = $tag;
+            }
         }
+        return $rating;
     }
 }
 
